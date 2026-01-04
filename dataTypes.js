f1c4b183719f0c0aa0341bae66796dcd7c431757
Admin <prachimshewale26@gmail.com>
2026-01-04 11:57:50 +0530

    basics

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+//primitive data types-normal variavle like int char in java -nn bb ss u -->null number boolean bigint string symbol undefined 
+let a="Harry"
+console.log(typeof a)
+
+//non primitive -objects in java script --key value pairs
+const obj={
+    "a":12,"b":13
+}
+console.log(obj)
+console.log(obj["a"])
+console.log(obj["x"])
---
+++ firstProgram.js
@@ -0,0 +1,12 @@
+console.log("hello their first java script program");
+let name="Prachi"; //allows us to chnage a type of variable at runtime
+console.log(name)
+name=34
+console.log(name);
+const age=90;  
+var a=10;//var declares a variable globally ..it can be used anywhere and can be chnagged
+{
+    let a= 20;//let is for block variables and we cant redeclare let
+    console.log(a)
+}
+console.log(a)
